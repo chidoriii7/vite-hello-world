40f14f1408f433aec5355d328f41cfdbf20de544
chidoriii7 <muratore.danilo@yahoo.it>
2024-05-29 23:03:20 +0200

    vite-hello-world

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'myApp',
+    data() {
+        return {
+            titolo: 'la mia prima app con vite-vue'
+        }
+    }
+};
+</script>
+
+
+<template>
+
+ <h1> {{ titolo }} </h1>
+
+</template>
+
+
+<style>
+h1{
+    color: blue;
+}
+</style>
